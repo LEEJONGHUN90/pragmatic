f41679f752c7fa3b746af261fbbd35c13e96c46e
Jonghun <ijin-ui@ijin-uiui-MacBook-Air.local>
2022-03-27 11:42:52 +0900

    django course 22 commit

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -15,6 +15,8 @@ from pathlib import Path
 import os, environ
 import os
 
+from django.urls import reverse_lazy
+
 env = environ.Env(
     # set casting, default value
     DEBUG=(bool, False)
@@ -137,6 +139,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+LOGIN_REDIRECT_URL= reverse_lazy('accountapp:hello_world')
+LOGOUT_REDIRECT_URL= reverse_lazy('accountapp:login')
 
 
 # Default primary key field type
