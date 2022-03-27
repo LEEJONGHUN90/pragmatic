a8e45db192ba3baf807d7e3d5d0e9c392e2659c8
Jonghun <ijin-ui@ijin-uiui-MacBook-Air.local>
2022-03-27 14:12:30 +0900

    Bug Fix

--- accountapp/views.py
+++ accountapp/views.py
@@ -14,19 +14,22 @@ from accountapp.models import HelloWorld
 
 def hello_world(request):
 
-    if request.method == "POST":
+    if request.user.is_authenticated:
 
-        temp = request.POST.get('hello_world_input')
+        if request.method == "POST":
 
-        new_hello_world = HelloWorld()
-        new_hello_world.text = temp
-        new_hello_world.save()
+            temp = request.POST.get('hello_world_input')
 
-        return HttpResponseRedirect(reverse('accountapp:hello_world'))
+            new_hello_world = HelloWorld()
+            new_hello_world.text = temp
+            new_hello_world.save()
+
+            return HttpResponseRedirect(reverse('accountapp:hello_world'))
+        else:
+            hello_world_list = HelloWorld.objects.all()
+            return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
     else:
-        hello_world_list = HelloWorld.objects.all()
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_list': hello_world_list})
-
+        return HttpResponseRedirect(reverse('accountapp:login'))
 
 
 class AccountCreateView(CreateView):
@@ -37,14 +40,14 @@ class AccountCreateView(CreateView):
 
 class AccountDetailView(DetailView):
     model = User
-    context_object_name='target_user'
+    context_object_name = 'target_user'
     template_name = 'accountapp/detail.html'
 
 
 
 class AccountUpdateView(UpdateView):
     model = User
-    context_object_name='target_user'
+    context_object_name = 'target_user'
     form_class = AccountUpdateForm
     success_url = reverse_lazy('accountapp:hello_world')
     template_name = 'accountapp/update.html'
@@ -52,6 +55,6 @@ class AccountUpdateView(UpdateView):
 
 class AccountDeleteView(DeleteView):
     model = User
-    context_object_name='target_user'
+    context_object_name = 'target_user'
     success_url = reverse_lazy('accountapp:login')
     template_name = 'accountapp/delete.html'
